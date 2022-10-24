1fddd6e93c577aac717414e99139bba40e8bf417
cankatmis <ackatmis@gmail.com>
2022-10-24 22:16:39 +0200

    Create Car.cs

---
+++ database-exercise/database-exercise/Models/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace database_exercise.Models
+{
+    [Table("cars")]
+    public class Car
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("car_id", TypeName = "int")]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string Model { get; set; }
+
+        public int? BasePrice { get; set; }
+
+        [ForeignKey(nameof(Brand))]
+        public int BrandId { get; set; }
+
+        [NotMapped]
+        public virtual Brand Brand { get; set; }
+
+    }
+}
